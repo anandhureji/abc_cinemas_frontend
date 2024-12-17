11005a59ad5b68b3ab095311c6403663912cc170
anandhureji <anandhureji33@gmail.com>
2024-12-17 21:18:38 +0530

    integrated with backend

--- src/components/MovieCardComponent/MovieCard.jsx
+++ src/components/MovieCardComponent/MovieCard.jsx
@@ -4,7 +4,7 @@ import './MovieCard.css'
 const MovieCard = ({ movie }) => {
     return (
         <div className="movie-card">
-            <img src={movie.imageUrl} alt={movie.title} className="movie-image" />
+            <img src={movie.image_data} alt={movie.title} className="movie-image" />
             <div className="movie-info">
                 <h3>{movie.title}</h3>
                 <p>Duration: {movie.duration} mins</p>
--- src/components/MovieCardComponent/MovieList.jsx
+++ src/components/MovieCardComponent/MovieList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import roudhiram from '../img/1538894691.jpg'
 import sooksha from '../img/481162833.jpg'
 import qalb from '../img/1731525465.jpg'
@@ -6,22 +6,39 @@ import pushpa from '../img/1492057053.jpg'
 import MovieCard from './MovieCard'
 import './MovieList.css'
 
-const movies = [
-    {id:1,title:"Rudhiram",duration:"1 hr 48 m",language:'Malayalam',imageUrl:roudhiram},
-    {id:2,title:"Pushpa",duration:"1 hr 48 m",language:'Tamil',imageUrl:pushpa},
-    {id:3,title:"Qalb",duration:"1 hr 48 m",language:'Malayalam',imageUrl:qalb},
-    {id:4,title:"SookshmaDarshini",duration:"1 hr 48 m",language:'Malayalam',imageUrl:sooksha}
+// const movies = [
+//     {id:1,title:"Rudhiram",duration:"1 hr 48 m",language:'Malayalam',imageUrl:roudhiram},
+//     {id:2,title:"Pushpa",duration:"1 hr 48 m",language:'Tamil',imageUrl:pushpa},
+//     {id:3,title:"Qalb",duration:"1 hr 48 m",language:'Malayalam',imageUrl:qalb},
+//     {id:4,title:"SookshmaDarshini",duration:"1 hr 48 m",language:'Malayalam',imageUrl:sooksha}
 
 
 
-]
+// ]
+
+
 
 
 const MoviesList = () => {
+
+    const [movies,setMovies] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:8080/api/movies/getActiveMovies')
+        .then(respose=>respose.json())
+        .then(data=>setMovies(data))
+        .catch(error=>console.error('Error',error));
+    },[]);
+    console.log(movies)
+
+
     return (
+        
         <div className="movies-list-container">
+            
             <h2 className="now-showing">Now Showing</h2>
             <div className="movies-list">
+            
                 {movies.map(movie => (
                     <MovieCard key={movie.id} movie={movie} />
                 ))}
